635facffad54187d420e6435e77539e62d825773
Giselle Quarto <gisellequarto@gmail.com>
2021-03-23 11:17:32 +0000

    schema migrations

--- versao-inicial/backend/migrations/20210323110810_create_table_users.js
+++ versao-inicial/backend/migrations/20210323110810_create_table_users.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('users', table => {
+        table.increments('id').primary()
+        table.string('name').notNull()
+        table.string('email').notNull().unique()
+        table.string('password').notNull()
+        table.boolean('admin').notNull().defaultTo(false)
+    })
 };
 
+// sempre faz o oposto do up. Se o up for deletar,aqui no down vc vai recriar a tabela
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTable('users')
 };
