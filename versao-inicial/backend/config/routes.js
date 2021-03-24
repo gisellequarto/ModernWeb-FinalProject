1dbfd71db1a396ff0b12767216c5cde70b72690a
Giselle Quarto <gisellequarto@gmail.com>
2021-03-24 17:27:02 +0000

    article

--- versao-inicial/backend/config/routes.js
+++ versao-inicial/backend/config/routes.js
@@ -19,14 +19,22 @@ module.exports = app => {
         .get(app.api.category.getTree)
 
 
-
     app.route('/categories/:id')
         .get(app.api.category.getById)
         .put(app.api.category.save)
         .delete(app.api.category.remove);
         //.all(app.config.passport.authenticate())
 
-    
+    app.route('/articles')
+        //.all(app.config.passport.authenticate())
+        .get(app.api.article.get)
+        .post(app.api.article.save)
+
+    app.route('/articles/:id')
+        //.all(app.config.passport.authenticate())
+        .get(app.api.article.getById)
+        .put(app.api.article.save)
+        .delete(app.api.article.remove)
 
 
 
