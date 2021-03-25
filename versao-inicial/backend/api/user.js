8c340a2edfe8aa77bb5dbd08ddfdd076e7f2afc2
Giselle Quarto <gisellequarto@gmail.com>
2021-03-25 14:11:54 +0000

    metodos do user deletedAt

--- versao-inicial/backend/api/user.js
+++ versao-inicial/backend/api/user.js
@@ -63,6 +63,7 @@ module.exports = app => {
         app.db('users')
             .select('id', 'name', 'email', 'admin')
             .where({ id: req.params.id })
+            .whereNull('deletedAt')
             .first()
             .then(user => res.json(user))
             .catch(err => res.status(500).send(err))
