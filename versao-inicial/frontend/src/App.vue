d737beec4ae078cf046777826273eb4eab3b2479
Giselle Quarto <gisellequarto@gmail.com>
2021-03-26 11:57:19 +0000

    style header

--- versao-inicial/frontend/src/App.vue
+++ versao-inicial/frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Header />
+    <Header title="Projeto Final - Web Moderno" :hideToggle="false"/>
     <Menu />
     <Content />
     <Footer />
--- versao-inicial/frontend/src/components/template/Header.vue
+++ versao-inicial/frontend/src/components/template/Header.vue
@@ -1,22 +1,73 @@
 <template>
-  <div class="header">HELLO</div>
+  <header class="header">
+    <a class="toggle" @click="toggleMenu" v-if="!hideToggle">
+      <i class="fa fa-lg" :class="icon"></i>
+    </a>
+    <h1 class="title">{{ title }}</h1>
+  </header>
 </template>
 
 <script>
 export default {
   name: "Header",
-};
+  props: {
+    title: String,
+    hideToggle: Boolean,
+  },
+  computed: {
+    icon() {
+      return "fa-angle-left"
+    }
+  },
+  methods: {
+    toggleMenu() {
+      
+    }
+  }
+}
 </script>
 
 <style>
 .header {
   grid-area: header;
-  /* background-color: #0093E9; */
-  background: linear-gradient(-206deg, #4b6cb7 5%, #182848 100%);
-  
+  background: linear-gradient(to right, #4b6cb7 5%, #182848 100%);
 
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.title {
+  font-size: 1.2rem;
+  color: #fff;
+  font-weight: 100;
+  flex-grow: 1;
+  text-align: center;
+}
+
+.title a {
+  color: #fff;
+  text-decoration: none;
+}
+
+.title a:hover {
+  color: #fff;
+  text-decoration: none;
+}
+
+header.header > a.toggle {
+  width: 60px;
+  height: 100%;
+  color: #fff;
+  justify-self: flex-start;
+  text-decoration: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header.header > a.toggle:hover {
+  color: #fff;
+  background-color: rgba(0, 0, 0, 0.2);
 }
 </style>
