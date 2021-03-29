1c7103c0d602932b4f3b2131b77152e325ac3f9c
Giselle Quarto <gisellequarto@gmail.com>
2021-03-29 17:28:09 +0100

    admin pages 1

--- versao-inicial/frontend/src/components/admin/AdminPages.vue
+++ versao-inicial/frontend/src/components/admin/AdminPages.vue
@@ -1,8 +1,25 @@
 <template>
   <div class="admin-pages">
-    <PageTitle icon="fa fa-cogs" main=" / Settings" 
-        sub="Register, Update & more..."
+    <PageTitle
+      icon="fa fa-cogs"
+      main=" / Settings"
+      sub="Register, Update & more..."
     />
+    <div class="admin-tab">
+      <b-card no-body>
+        <b-tabs card>
+          <b-tab title="Artigos" active>
+            <ArticleAdmin />
+          </b-tab>
+          <b-tab title="Categorias">
+            <CategoryAdmin />
+          </b-tab>
+          <b-tab title="Usuários">
+            <UserAdmin />
+          </b-tab>
+        </b-tabs>
+      </b-card>
+    </div>
   </div>
 </template>
 
@@ -16,4 +33,5 @@ export default {
 </script>
 
 <style>
+
 </style>
