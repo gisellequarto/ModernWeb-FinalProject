e029e0d4ae0ca0ffb459e6c28d3dc87e1e3d1af7
Giselle Quarto <gisellequarto@gmail.com>
2021-04-01 18:22:22 +0100

    auth and responsive

--- versao-inicial/frontend/src/main.js
+++ versao-inicial/frontend/src/main.js
@@ -6,9 +6,11 @@ import App from './App';
 
 import './config/bootstrap';
 import './config/msgs';
+import './config/axios';
+import './config/mq';
 
 import store from './config/store';
-import router from './config/router'
+import router from './config/router';
 
 Vue.config.productionTip = false;
 
